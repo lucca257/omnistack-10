dc94f0583f3da325eeeb7cd1ab0f85da05ffa666
luccazx <pedrolucca257@gmail.com>
2020-02-15 10:14:58 -0300

    web module

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header';
+function App() {
+  return (
+    <>
+      <Header title="título 1"/>
+      <Header title="título 2"/>
+      <Header title="título 3"/>
+    </>
+  );
+}
+
+export default App;
