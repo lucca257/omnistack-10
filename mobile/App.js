491b62d9dfe6c15dcf7f288aeea022dcf7beedc9
pedro lucca <pedrolucca257@gmail.com>
2020-02-26 17:23:28 -0300

    aula 4

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Routes from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Semana OmniStack</Text>
-    </View>
+    <>
+      <StatusBar/>
+      <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: "bold",
-    fontSize: 32,
-    color: "#FFF"
-  }
-});
